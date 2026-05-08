1024995ff0007913e2b49f6ac06ff044926130f5
TuranX857 <quliyevturan857@gmail.com>
2026-05-08 18:29:03 +0000

    salam

--- cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/4-root_process.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps -u "$1" -o user,pid,%cpu,%mem,vsz,rss,tty,stat,start,time,command --no-headers | grep -vE '^([^[:space:]]+[[:space:]]+){4}0[[:space:]]+0[[:space:]]|grep'
+ps aux | grep "^$1 " | grep -v " 0  *0 "
